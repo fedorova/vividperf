d2fa28b7b83a3a105c03cf8309db6c0894af49f4
Sasha Fedorova <sasha@wiredtiger.com>
2014-03-20 10:39:28 -0700

    A couple of typos in scripts

--- pintools/scripts/showprocs-static.sh
+++ pintools/scripts/showprocs-static.sh
@@ -4,4 +4,4 @@ env LD_LIBRARY_PATH=../wt-dev/build_posix/.libs:../wt-dev/build_posix/ext/compre
 
 
 # Read from the DB randomly
-env LD_LIBRARY_PATH=../wt-dev/build_posix/.libs:../wt-dev/build_posix/ext/compressors/snappy/.libs/  pin.sh  -t $CUSTOM_PINTOOLS_HOME/obj-intel64/procinstr.so -i procs.in --  ./db_bench_wiredtiger --cache_size=534217728  --use_existing_db=1 --threads=1 --use_lsm=0 --db=/tmpfs/leveldb --reads=1000000 --benchmarks=readrandom --value_size=62 
+env LD_LIBRARY_PATH=../wt-dev/build_posix/.libs:../wt-dev/build_posix/ext/compressors/snappy/.libs/  pin.sh  -t $CUSTOM_PINTOOLS_HOME/obj-intel64/showprocs-static.so  --  ./db_bench_wiredtiger --cache_size=534217728  --use_existing_db=1 --threads=1 --use_lsm=0 --db=/tmpfs/leveldb --reads=1000000 --benchmarks=readrandom --value_size=62 
